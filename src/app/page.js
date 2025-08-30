d51f8d98510a32500aeb5e088c01e8b243a83f75
rahulvishwakarma-coder <rahulvishwakarma88872@gmail.com>
2025-08-30 12:26:56 +0530

    Version 3.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
   'use client'
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
-  const [count,setcount] = useState(1);
   return (
     <div>
-      <button onClick={() =>setcount(count +1)} className=" cursor-pointer bg-amber-100 text-white rounded-xl">click</button>
-      <div>{count} </div>
     </div>
   );
 }
